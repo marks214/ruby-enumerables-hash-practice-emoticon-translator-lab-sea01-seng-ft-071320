9372d6b1f6ebb68d29d6120efc04a7e65a58e127
Aimee Oz <aimee.oz.x@gmail.com>
2020-06-13 22:55:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,7 @@ def load_library(thing = THING)
       if !final_hash[append]
         final_hash[append] = {}
       end
-      final_hash[append] = value
+      final_hash[append] = [value]
     end
   end
 #puts "dictionary #{dictionary}"
