d005c819a26cdc5aca9fb0f52684c69a5e8a0c69
Aimee Oz <aimee.oz.x@gmail.com>
2020-06-13 22:31:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,12 +1,12 @@
 # require modules here
 require 'pry'
 require 'yaml'
-THING = YAML.load_file('lib/emoticons.yml')
+THING = YAML.load_file('emoticons.yml')
 
 def load_library(thing = THING)
   # code goes here
   library = thing
-  binding.pry 
+  binding.pry
 end
 
 def get_japanese_emoticon(thing = THING)
