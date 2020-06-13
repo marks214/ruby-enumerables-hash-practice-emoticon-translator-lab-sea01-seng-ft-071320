c5ae67c00629d138689211810b53e69317f23ae3
Aimee Oz <aimee.oz.x@gmail.com>
2020-06-13 22:37:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,10 +9,14 @@ def load_library(thing = THING)
   return library
 end
 
-def get_japanese_emoticon(thing = THING)
+def get_japanese_emoticon(emotion)
   # code goes here
+  load_library
 end
 
-def get_english_meaning(thing = THING)
-  # code goes here
+def get_english_meaning(emotion)
+  dictionary = load_library
+  dictionary.each do |key, value|
+    binding.pry
+  end
 end
